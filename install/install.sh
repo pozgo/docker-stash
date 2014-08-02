4704af57be966863f1f0f9c46f3dfac2f96ff3ce
Przemyslaw Ozgo <linux@ozgo.info>
2014-08-02 14:51:19 +0100

    Added install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Starting installation of Confluence..."
 cd /data 
-wget -q http://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-5.5.3-x64.bin
-chmod +x atlassian-confluence-5.5.3-x64.bin
-./atlassian-confluence-5.5.3-x64.bin <<<"o
+wget -q http://www.atlassian.com/software/stash/downloads/binary/atlassian-stash-3.2.0-x64.bin
+chmod +x atlassian-stash-3.2.0-x64.bin
+./atlassian-stash-3.2.0-x64.bin <<<"o
 1
 "
